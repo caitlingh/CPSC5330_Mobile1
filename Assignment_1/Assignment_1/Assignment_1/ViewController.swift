c4166307beb9f741fbab635e046371eac186a5b0
Caitlin Hendricks <caitlingh@caitlins-laptop.lan>
2025-05-24 11:22:02 -0500

    created assignment 1

---
+++ Assignment_1/Assignment_1/Assignment_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_1
+//
+//  Created by Caitlin Hendricks on 5/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
