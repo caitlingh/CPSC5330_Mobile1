123d10b000440663a7affa11e679c96ee7220d80
Caitlin Hendricks <caitlingh@mac.lan>
2025-06-28 22:29:18 -0500

    implemented program logic

--- Assignment_6/Assignment_6/ConversionViewController.swift
+++ Assignment_6/Assignment_6/ConversionViewController.swift
@@ -1,20 +1,81 @@
 //
-//  ConversionViewController.swift
+//  ConversionViewController 2.swift
 //  Assignment_6
 //
 //  Created by Caitlin Hendricks on 6/28/25.
 //
 
+
 import UIKit
 
 class ConversionViewController: UIViewController {
 
+    @IBOutlet weak var usdAmountLabel: UILabel!
+    
+    @IBOutlet weak var eurLabel: UILabel!
+    @IBOutlet weak var jpyLabel: UILabel!
+    @IBOutlet weak var gbpLabel: UILabel!
+    @IBOutlet weak var cadLabel: UILabel!
+    @IBOutlet weak var eurAmountLabel: UILabel!
+    @IBOutlet weak var jpyAmountLabel: UILabel!
+    @IBOutlet weak var gbpAmountLabel: UILabel!
+    @IBOutlet weak var cadAmountLabel: UILabel!
+    
+    // user input
+    var usd : Int = 0
+    
+    // display currency conversions
+    var showEUR = false
+    var showJPY = false
+    var showGBP = false
+    var showCAD = false
+    
+    // currency exchange rates
+    var eurRate : Double = 0.85
+    var jpyRate : Double = 145.00
+    var gbpRate : Double = 0.74
+    var cadRate : Double = 1.37
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        usdAmountLabel.text = "$\(usd)"
+        
+        if showEUR {
+            let eur = Double(usd) * eurRate
+            eurAmountLabel.text = "€\(String(format: "%.2f", eur))"
+        } else {
+            eurLabel.isHidden = true
+            eurAmountLabel.isHidden = true
+        }
+        
+        if showJPY {
+            let jpy = Double(usd) * jpyRate
+            jpyAmountLabel.text = "¥\(String(format: "%.2f", jpy))"
+        } else {
+            jpyLabel.isHidden = true
+            jpyAmountLabel.isHidden = true
+        }
+        
+        if showGBP {
+            let gbp = Double(usd) * gbpRate
+            gbpAmountLabel.text = "£\(String(format: "%.2f", gbp))"
+        } else {
+            gbpLabel.isHidden = true
+            gbpAmountLabel.isHidden = true
+        }
+        
+        if showCAD {
+            let cad = Double(usd) * cadRate
+            cadAmountLabel.text = "C$\(String(format: "%.2f", cad))"
+        } else {
+            cadLabel.isHidden = true
+            eurAmountLabel.isHidden = true
+        }
+        
 
         // Do any additional setup after loading the view.
     }
-    
 
     /*
     // MARK: - Navigation
@@ -25,5 +86,7 @@ class ConversionViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
 
 }
--- Assignment_6/Assignment_6/ViewController.swift
+++ Assignment_6/Assignment_6/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var errorMessage: UILabel!
+    @IBOutlet weak var usdInput: UITextField!
+    
+    // user input
+    var usd : Int = 0
+    
+    // switch toggles
+    var eurSwitch = true
+    var jpySwitch = true
+    var gbpSwitch = true
+    var cadSwitch = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +28,58 @@ class ViewController: UIViewController {
     }
 
     @IBAction func calculate(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "toConversion", sender: self)
+        if let text = usdInput.text, let value = Int(text) {
+            usd = value
+            errorMessage.isHidden = true
+            self.performSegue(withIdentifier: "toConversion", sender: self)
+        } else {
+            errorMessage.isHidden = false
+        }
+    }
+    
+    @IBAction func euro(_ sender: UISwitch) {
+        if sender.isOn {
+            eurSwitch = true
+        } else {
+            eurSwitch = false
+        }
+    }
+    
+    @IBAction func yen(_ sender: UISwitch) {
+        if sender.isOn {
+            jpySwitch = true
+        } else {
+            jpySwitch = false
+        }
+    }
+    
+    @IBAction func pound(_ sender: UISwitch) {
+        if sender.isOn {
+            gbpSwitch = true
+        } else {
+            gbpSwitch = false
+        }
+    }
+    
+    @IBAction func canadian(_ sender: UISwitch) {
+        if sender.isOn {
+            cadSwitch = true
+        } else {
+            cadSwitch = false
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-//        if segue.identifier == "toConversion" {
-//            let calculation = segue.destination
-//        }
+        if segue.identifier == "toConversion",
+               let calculation = segue.destination as? ConversionViewController {
+                calculation.usd = usd
+                calculation.showEUR = eurSwitch
+                calculation.showJPY = jpySwitch
+                calculation.showGBP = gbpSwitch
+                calculation.showCAD = cadSwitch
+            }
     }
     
 }
