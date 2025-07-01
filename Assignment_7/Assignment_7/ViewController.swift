97bd80b2636b2bcedbbcf87bd18c5ef7b1bb12e5
Caitlin Hendricks <caitlingh@mac.lan>
2025-07-01 14:53:46 -0500

    UI design complete

---
+++ Assignment_7/Assignment_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment_7
+//
+//  Created by Caitlin Hendricks on 7/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
